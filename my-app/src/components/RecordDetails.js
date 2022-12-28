c5daeb5d01c0bf8f3d7a276c30dba048e6e5c96d
Sienna <remicknsisi@gmail.com>
2022-12-28 11:27:08 -0500

    added some styling to the details page

--- my-app/src/components/RecordDetails.js
+++ my-app/src/components/RecordDetails.js
@@ -5,21 +5,27 @@ function RecordDetails({ records }) {
   const { id } = useParams()
 
   const recordDetails = records.find(record => record.id == id)
-  console.log(recordDetails)
 
   return (
-    <div className="record-details">
-        <img src={recordDetails.image} alt={recordDetails.image} />
-        <h1>{recordDetails.title}</h1>
-        <h3>{recordDetails.artist}</h3>
-        <ul>Side A Tracks:</ul>
-            {recordDetails.sideA.map(song =>
-                <li key={song}>{song}</li>
-            )}
-        <ul>Side B Tracks:</ul>
-        {recordDetails.sideB.map(song =>
-                <li key={song}>{song}</li>
-            )}
+    <div>
+        <div className="record-details">
+            <img src={recordDetails.image} alt={recordDetails.image} />
+            <h1>{recordDetails.title}</h1>
+            <h3>{recordDetails.artist}</h3>
+        </div>
+        <div className="tracks-container">
+            <ul className="tracks">Side A Tracks:
+                {recordDetails.sideA.map(song =>
+                    <li key={song}>{song}</li>
+                )}
+            </ul>
+            <ul className="tracks">Side B Tracks:
+            {recordDetails.sideB.map(song =>
+                    <li key={song}>{song}</li>
+                )}
+            </ul>
+        </div>
+        <br></br>
     </div>
   );
 }
